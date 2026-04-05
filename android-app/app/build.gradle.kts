88f05d1d594529659d33d9c40dad82e9af380316
NOAHSDELLLAPTOP\noaha <noahafriat11@gmail.com>
2026-04-05 00:13:50 -0400

    CI fix (add espresso-intents library)

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -4,16 +4,11 @@ plugins {
 
 android {
     namespace = "com.example.ticketreservationapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
-
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.ticketreservationapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -47,5 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.espresso.intents)
 
 }
